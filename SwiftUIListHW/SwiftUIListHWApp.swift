65184514779083b452d6b1164d633924b20affce
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-11 11:20:15 +0800

    Initial Commit

---
+++ SwiftUIListHW/SwiftUIListHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListHWApp.swift
+//  SwiftUIListHW
+//
+//  Created by 廖昱晴 on 2021/3/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
